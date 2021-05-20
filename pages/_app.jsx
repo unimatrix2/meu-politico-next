8649c24405e8f02ed69545782f46b15aabb8f420
unimatrix2 <dnny.nis@gmail.com>
2021-05-19 23:26:45 -0300

    added context provider

---
+++ pages/_app.jsx
@@ -0,0 +1,12 @@
+import '../styles/globals.css';
+import { Provider } from '../context/Context';
+
+function MyApp({ Component, pageProps }) {
+	return (
+		<Provider>
+			<Component {...pageProps} />
+		</Provider>
+	);
+}
+
+export default MyApp;
